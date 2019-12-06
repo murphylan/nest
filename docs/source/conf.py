35f39d98204b15fafb49c782d82262065e8f9d4f
lanzejun <lanzejun@cn.ibm.com>
2019-12-06 10:42:00 +0800

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,11 +16,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-# import yummy_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'FSD 培训'
+project = 'nest'
 copyright = '2019, Murphy'
 author = 'Murphy'
 
@@ -86,8 +85,6 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-# html_theme = "yummy_sphinx_theme"
-# html_theme_path = ["_themes/yummy_sphinx_theme", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -103,7 +100,6 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -127,182 +123,62 @@ html_css_files = [
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'nestdoc'
 
-# -- Options for LaTeX output ---------------------------------------------
 
-latex_engine = 'pdflatex'
+# -- Options for LaTeX output ------------------------------------------------
+
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    'papersize': 'a4paper',
-    'releasename':" ",
-    # Sonny, Lenny, Glenn, Conny, Rejne, Bjarne and Bjornstrup
-    # 'fncychap': '\\usepackage[Lenny]{fncychap}',
-    'fncychap': '\\usepackage{fncychap}',
-    'fontpkg': '\\usepackage{amsmath,amsfonts,amssymb,amsthm}',
+    # 'papersize': 'letterpaper',
 
-    'figure_align':'htbp',
     # The font size ('10pt', '11pt' or '12pt').
     #
-    'pointsize': '10pt',
+    # 'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble': r'''
-        %%%%%%%%%%%%%%%%%%%% Meher %%%%%%%%%%%%%%%%%%
-        %%%add number to subsubsection 2=subsection, 3=subsubsection
-        %%% below subsubsection is not good idea.
-        \setcounter{secnumdepth}{3}
-        %
-        %%%% Table of content upto 2=subsection, 3=subsubsection
-        \setcounter{tocdepth}{2}
+    # 'preamble': '',
 
-        \usepackage{amsmath,amsfonts,amssymb,amsthm}
-        \usepackage{graphicx}
-
-        %%% reduce spaces for Table of contents, figures and tables
-        %%% it is used "\addtocontents{toc}{\vskip -1.2cm}" etc. in the document
-        \usepackage[notlot,nottoc,notlof]{}
-
-        \usepackage{color}
-        \usepackage{transparent}
-        \usepackage{eso-pic}
-        \usepackage{lipsum}
-
-        \usepackage{footnotebackref} %%link at the footnote to go to the place of footnote in the text
-
-        %% spacing between line
-        \usepackage{setspace}
-        %%%%\onehalfspacing
-        %%%%\doublespacing
-        \singlespacing
-
-
-        %%%%%%%%%%% datetime
-        \usepackage{datetime}
-
-        \newdateformat{MonthYearFormat}{%
-            \monthname[\THEMONTH], \THEYEAR}
-
-
-        %% RO, LE will not work for 'oneside' layout.
-        %% Change oneside to twoside in document class
-        \usepackage{fancyhdr}
-        \pagestyle{fancy}
-        \fancyhf{}
-
-        %%% Alternating Header for oneside
-        \fancyhead[L]{\ifthenelse{\isodd{\value{page}}}{ \small \nouppercase{\leftmark} }{}}
-        \fancyhead[R]{\ifthenelse{\isodd{\value{page}}}{}{ \small \nouppercase{\rightmark} }}
-
-        %%% Alternating Header for two side
-        %\fancyhead[RO]{\small \nouppercase{\rightmark}}
-        %\fancyhead[LE]{\small \nouppercase{\leftmark}}
-
-        %% for oneside: change footer at right side. If you want to use Left and right then use same as header defined above.
-        \fancyfoot[R]{\ifthenelse{\isodd{\value{page}}}{{\tiny Meher Krishna Patel} }{\href{http://pythondsp.readthedocs.io/en/latest/pythondsp/toc.html}{\tiny PythonDSP}}}
-
-        %%% Alternating Footer for two side
-        %\fancyfoot[RO, RE]{\scriptsize Meher Krishna Patel (mekrip@gmail.com)}
-
-        %%% page number
-        \fancyfoot[CO, CE]{\thepage}
-
-        \renewcommand{\headrulewidth}{0.5pt}
-        \renewcommand{\footrulewidth}{0.5pt}
-
-        \RequirePackage{tocbibind} %%% comment this to remove page number for following
-        \addto\captionsenglish{\renewcommand{\contentsname}{Table of contents}}
-        \addto\captionsenglish{\renewcommand{\listfigurename}{List of figures}}
-        \addto\captionsenglish{\renewcommand{\listtablename}{List of tables}}
-        % \addto\captionsenglish{\renewcommand{\chaptername}{Chapter}}
-
-
-        %%reduce spacing for itemize
-        \usepackage{enumitem}
-        \setlist{nosep}
-
-        %%%%%%%%%%% Quote Styles at the top of chapter
-        \usepackage{epigraph}
-        \setlength{\epigraphwidth}{0.8\columnwidth}
-        \newcommand{\chapterquote}[2]{\epigraphhead[60]{\epigraph{\textit{#1}}{\textbf {\textit{--#2}}}}}
-        %%%%%%%%%%% Quote for all places except Chapter
-        \newcommand{\sectionquote}[2]{{\quote{\textit{``#1''}}{\textbf {\textit{--#2}}}}}
-    ''',
-
-
-    'maketitle': r'''
-        \pagenumbering{Roman} %%% to avoid page 1 conflict with actual page 1
-
-        \begin{titlepage}
-            \centering
-
-            \vspace*{40mm} %%% * is used to give space from top
-            \textbf{\Huge {Sphinx format for Latex and HTML}}
-
-            \vspace{0mm}
-            \begin{figure}[!h]
-                \centering
-                \includegraphics[scale=0.3]{logo.jpg}
-            \end{figure}
-
-            \vspace{0mm}
-            \Large \textbf{{Meher Krishna Patel}}
-
-            \small Created on : Octorber, 2017
-
-            \vspace*{0mm}
-            \small  Last updated : \MonthYearFormat\today
-
-
-            %% \vfill adds at the bottom
-            \vfill
-            \small \textit{More documents are freely available at }{\href{http://pythondsp.readthedocs.io/en/latest/pythondsp/toc.html}{PythonDSP}}
-        \end{titlepage}
-
-        \clearpage
-        \pagenumbering{roman}
-        \tableofcontents
-        \listoffigures
-        \listoftables
-        \clearpage
-        \pagenumbering{arabic}
-
-        ''',
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-    'sphinxsetup': \
-        'hmargin={0.7in,0.7in}, vmargin={1in,1in}, \
-        verbatimwithframe=true, \
-        TitleColor={rgb}{0,0,0}, \
-        HeaderFamily=\\rmfamily\\bfseries, \
-        InnerLinkColor={rgb}{0,0,1}, \
-        OuterLinkColor={rgb}{0,0,1}',
-
-        'tableofcontents':' ',
-
-
-
 }
-
-latex_logo = '_static/sphinx.svg'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'main.tex', 'Sphinx format for Latex and HTML',
-     'Meher Krishna Patel', 'report')
+    (master_doc, 'nest.tex', '',
+     'Murphy', 'manual'),
 ]
 
 
-# -- Options for Epub output ----------------------------------------------
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'nest', '',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output ----------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'nest', '',
+     author, 'nest', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+
+# -- Options for Epub output -------------------------------------------------
 
 # Bibliographic Dublin Core info.
 epub_title = project
-epub_author = author
-epub_publisher = author
-epub_copyright = copyright
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
@@ -315,21 +191,6 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
-
-
-def setup(app):
-    app.add_stylesheet('custom.css')  # remove line numbers
-    app.add_javascript('copybutton.js') # show/hide prompt >>>
-
-# use :numref: for references (instead of :ref:)
-numfig = True
-smart_quotes = False
-html_use_smartypants = False
 
 
 # -- Extension configuration -------------------------------------------------
